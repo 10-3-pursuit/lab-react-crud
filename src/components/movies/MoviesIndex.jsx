a32dc1a288dc5af9f672e08353020d33488acd21
enochtagoe <enochtagoe@pursuit.org>
2024-01-04 14:40:11 -0500

    initial commit

--- src/components/movies/MoviesIndex.jsx
+++ src/components/movies/MoviesIndex.jsx
@@ -1,3 +1,34 @@
+import { Link } from "react-router-dom";
+
+import ErrorMessage from "../errors/ErrorMessage";
+
+import "./MoviesIndex.css";
+
 export default function MoviesIndex() {
-  return <p>Movie List</p>;
-}
+  return (
+  <div>
+  {false ? (
+    <ErrorMessage />
+  ) : (
+<section className="movies-index-wrapper">
+  <p>Movie List</p>
+<button>
+<Link to="/movies/new">Add a new Movie</Link>
+</button>
+<br />
+<label htmlFor="searchTitle">
+  Search Movies:
+  <input
+  type="text"
+id="searchTitle"
+  />
+</label>
+<section className="movies-index">
+
+</section>
+  </section>
+
+  )}
+  </div>
+  );
+};
