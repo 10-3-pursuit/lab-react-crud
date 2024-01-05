78fde9020c911a6b586c801deb85ce5d1031b54c
juligarc91 <julissagarcia@pursuit.org>
2024-01-04 20:11:38 -0500

    Added return elements to movie form

--- src/components/movies/MoviesNewForm.jsx
+++ src/components/movies/MoviesNewForm.jsx
@@ -14,9 +14,89 @@ const MoviesNewForm = () => {
     rating: "",
     releaseYear: "",
   });
+
+  function handleSubmitMovie () {};
+  function handleTextChangeMovie () {};
+
   return (
-    <div>MoviesNewForm</div>
-  )
+    <form onSubmit={handleSubmitMovie}>
+        <label htmlFor="title">Title:</label>
+      <input
+        type="text"
+        id="title"
+        value={show.title}
+        onChange={handleTextChangeMovie}
+      />
+
+      <label htmlFor="description">Description:</label>
+      <input
+        type="text"
+        id="description"
+        value={movie.description}
+        onChange={handleTextChangeMovie}
+      />
+
+      <label htmlFor="type">Type</label>
+      <input
+        type="text"
+        id="type"
+        value={movie.type}
+        onChange={handleTextChangeMovie}
+      />
+
+      <label htmlFor="rating">Rating:</label>
+      <input
+        type="text"
+        id="rating"
+        value={movie.rating}
+        onChange={handleTextChangeMovie}
+      />
+
+      <label htmlFor="listedIn">Listed in</label>
+      <input
+        type="text"
+        id="listedIn"
+        value={movie.listedIn}
+        onChange={handleTextChange}
+      />
+
+      <label htmlFor="duration">Duration</label>
+      <input
+        type="text"
+        id="duration"
+        value={movie.duration}
+        onChange={handleTextChange}
+      />
+
+      <label htmlFor="releaseYear">Release Year</label>
+      <input
+        type="text"
+        id="releaseYear"
+        value={movie.releaseYear}
+        onChange={handleTextChange}
+      />
+
+      <label htmlFor="country">Country</label>
+      <input
+        type="text"
+        id="country"
+        value={movie.country}
+        onChange={handleTextChange}
+      />
+
+      <label htmlFor="dateAdded">Date added:</label>
+      <input
+        type="text"
+        id="dateAdded"
+        value={movie.dateAdded}
+        onChange={handleTextChange}
+      />
+
+      <br />
+
+      <input type="submit" />
+    </form>
+  );
 }
 
 export default MoviesNewForm;
